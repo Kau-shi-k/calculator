5f089edcd761bdd625a1766a59e09ea5c3d591b9
ojas kaushik <ojaskaushik99@gmail.com>
2025-01-17 22:19:33 +0530

    Modified Code

--- src/division.java
+++ src/division.java
@@ -2,11 +2,29 @@ import java.util.Scanner;
 
 public class division {
 
-
-    public static double divide(int num1, int num2) {
+    public static double divideNumbers(double num1, double num2) {
         if (num2 == 0) {
             throw new ArithmeticException("Division by zero is not allowed.");
         }
-        return (double) num1 / num2;
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = sc.nextDouble();
+
+        try {
+            double result = divideNumbers(num1, num2);
+            System.out.println("Result of division: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
     }
 }
